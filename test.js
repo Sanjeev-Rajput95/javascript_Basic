63d61ae3cc5e2b012fd4d92493929a5c9f244eca
sanjeev <sanjeevsingh.77d@gmail.com>
2022-08-21 15:55:19 +0530

    Check number Even or Not

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-let a = 23;
-let b = 43;
-let add = a + b ;
-console.log(add);
+//console.log("Print hello world");
+//let a = 23;
+//let b = 43;
+//let add = a + b;
+//console.log(add);
+
+const prompt = require("prompt-sync")();
+let num = prompt("Enter the number") ;
+if(num%2==0) {
+    console.log("isEven");
+} 
+else {
+    console.log("notEven");
+}
