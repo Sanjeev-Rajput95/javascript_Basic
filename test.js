e86343412bc12acd25cb41ffcbacf0bc629420ba
sanjeev <sanjeevsingh.77d@gmail.com>
2022-08-21 16:08:14 +0530

    Division of two number

--- test.js
+++ test.js
@@ -1,14 +1,24 @@
+//1. ---------first program---printing....---
 //console.log("Print hello world");
+
+// 2.----------...adding two number----
 //let a = 23;
 //let b = 43;
 //let add = a + b;
 //console.log(add);
 
-const prompt = require("prompt-sync")();
-let num = prompt("Enter the number") ;
-if(num%2==0) {
-    console.log("isEven");
-} 
-else {
-    console.log("notEven");
-}
+// 3.-----check number is even or not-------
+//const prompt = require("prompt-sync")();
+//let num = prompt("Enter the number") ;
+//if(num%2==0) {
+  //  console.log("isEven");
+//} 
+//else {
+  //  console.log("notEven");
+//}
+
+// 4.-----Divide two number------
+let a = 25;
+let b = 5;
+let div = a/b;
+console.log(div);
