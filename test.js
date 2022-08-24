a274f7c64108aa1906093e0e3aa259ddb9470280
sanjeev <sanjeevsingh.77d@gmail.com>
2022-08-24 21:08:59 +0530

    Check Number Prime or not

--- test.js
+++ test.js
@@ -1,84 +1,116 @@
 //1. ---------first program---printing....---
-//console.log("Print hello world");
+console.log("Print hello world");
 
 // 2.----------...adding two number----
-//let a = 23;
-//let b = 43;
-//let add = a + b;
-//console.log(add);
+let a = 23;
+let b = 43;
+let add = a + b;
+console.log(add);
 
 // 3.-----check number is even or not-------
-//const prompt = require("prompt-sync")();
-//let num = prompt("Enter the number") ;
-//if(num%2==0) {
-  //  console.log("isEven");
-//} 
-//else {
-  //  console.log("notEven");
-//}
+const prompt = require("prompt-sync")();
+let num = prompt("Enter the number") ;
+if(num%2==0) {
+    console.log("isEven");
+} 
+else {
+    console.log("notEven");
+}
 
 // 4.-----Divide two number------
 
-//let a = 24;
- //let b = 3;
- //let div = a/b;
- //console.log(div);
+let l = 24;
+ let m = 3;
+ let div = l/m;
+ console.log(div);
 
  // 5.--------Find square------
 
  //const prompt = require("prompt-sync")();
- //let num = prompt("Enter the number");
- //let sqr = num*num;
- //console.log("Square of the number  = ",sqr);
+ let number = prompt("Enter the number");
+ let sqr = number*number;
+ console.log("Square of the number  = ",sqr);
 
  // 6. ---------Calculate the area of triangle-------
 
- // const prompt = require("prompt-sync")();
-  //let radius = prompt("Enter the  radius : ");
-  //let area = 3.14*radius*radius;
-  //console.log("Area of a triangle = ",area);
+  // const prompt = require("prompt-sync")();
+  let radius = prompt("Enter the  radius : ");
+  let area = 3.14*radius*radius;
+  console.log("Area of a triangle = ",area);
 
   // 7.--------Swap two number ------------
 
-  //let a = 3;
-  //let b =4;
-  //let temp;
-  //temp = a;
-  //a = b;
-  //b = temp;
-  //console.log(a,b);
+  let x = 3;
+  let y =4;
+  let temp;
+  temp = x;
+  x = y;
+  y = temp;
+  console.log(x,y);
 
   // 8.-------Swap two number without using third variable...
 
-  //let a = 5;
-  //let b = 2;
-  //b = a + b;
-  //a = b - a;
-  //b = b - a;
-  //console.log(a,b);
+  let c = 5;
+  let d = 2;
+  d = c + d;
+  c = d - c;
+  d = d - c;
+  console.log(c,d);
 
   // 9.------Find the Largest Among Three Numbers..
 
- // let a ;
-  //let b ;
-  //let c ;
-  //  if(a>b) {
-  //     if(a>c) {
-  //     console.log("a is the largest");
- //   } else {
- //           console.log("c is the largest");
-//     }
-//    } else {
-//      if(b>c) {
-//          console.log("b is the largest");
-//      } else {
-//          console.log("c is the largest");
-//      }
-//  }
+  let p ;
+  let q ;
+  let r;
+    if(p>q) {
+       if(p>r) {
+       console.log("a is the largest");
+    } else {
+            console.log("c is the largest");
+     }
+    } else {
+      if(q>r) {
+          console.log("b is the largest");
+      } else {
+          console.log("c is the largest");
+      }
+  }
 
 // 10. ------Convert Celsius to Fahrenheit..
 
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 let Celsius = prompt("Enter the value of Celsius : ");
 let Fahrenheit = Celsius*1.8 + 32;
 console.log(Fahrenheit);
+
+// 11. -------Prime Number......
+
+// const prompt = require("prompt-sync")();
+let n = prompt("Enter the number :");
+let  i, flag = true;
+             
+// Getting the value form text
+// field using DOM
+// n = document.myform.n.value;
+// n = parseInt(n)
+for(i = 2; i <= n - 1; i++)
+    if (n % i == 0) {
+        flag = false;
+        break;
+    }
+     
+    // Check and display alert message
+    
+if(n == 1 && n < 1) {
+    console.log(n + "  not prime")
+} else {
+    if (flag == true) {
+        console.log(n + " is prime");
+    }
+        
+  //  else
+    //    console.log(n + " is not prime");    
+}
+
+// 12.-----------Find Palindrom number--------
+
